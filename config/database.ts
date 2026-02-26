b0e5e0e176e943010268dc2f0790a43cb1486a43
Walizki Lublin <walizkilublin@gmail.com>
2026-02-26 19:34:15 +0100

    poprawa bazy danych

--- config/database.ts
+++ config/database.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import type { Core } from '@strapi/strapi';
 
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('DATABASE_URL') ? 'postgres' : env('DATABASE_CLIENT', 'sqlite');
 
   const connections = {
     mysql: {
